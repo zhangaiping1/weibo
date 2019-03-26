2db212960e6e18e4927c84f3c3c5b7e0d68ae096
zhangaiping1 <zhangaiping11011@163.com>
2019-03-26 01:23:07 +0000

    中文的错误提示信息

---
+++ resources/lang/zh-CN/demo.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'user_not_exists'    => '用户不存在',
+    'email_has_registed' => '邮箱 :email 已经注册过！',
+];
---
+++ resources/lang/zh-CN/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'reset' => '您的密码已经重置成功了，你可以使用新的密码登录了!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that e-mail address.",
+
+];
